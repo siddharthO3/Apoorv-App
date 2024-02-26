31a078a16f9d5bfd3e6d2031d831ec418b99defb
siddharth <guptasiddharth689@gmail.com>
2024-02-26 23:31:02 +0530

    Firebase Initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:apoorv_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  await DotEnv().load(fileName: '.env');
+  
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: await DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
