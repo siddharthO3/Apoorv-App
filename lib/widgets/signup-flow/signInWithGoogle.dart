53746b4d28723d4b067f1fd26f81be80373821db
siddharth <guptasiddharth689@gmail.com>
2024-02-27 18:12:23 +0530

    Some Folder structure added

---
+++ lib/widgets/signup-flow/signInWithGoogle.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SignInWithGoogle extends StatelessWidget {
+  const SignInWithGoogle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
