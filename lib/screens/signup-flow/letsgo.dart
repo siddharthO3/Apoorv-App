d1afc01dc4ce2a604b0cffe052874424f6b3ac4b
PranavSusar <pranavsusar@gmail.com>
2024-03-19 00:04:22 +0530

    let's go and sign up

--- lib/screens/signup-flow/letsgo.dart
+++ lib/screens/signup-flow/letsgo.dart
@@ -4,7 +4,6 @@ import 'package:apoorv_app/screens/signup-flow/welcome.dart';
 import 'package:flutter/material.dart';
 import '../../../constants.dart';
 
-
 class LetsGoPage extends StatelessWidget {
   static const routeName = "/letsgo";
   const LetsGoPage({super.key});
@@ -27,9 +26,61 @@ class LetsGoPage extends StatelessWidget {
                   fontWeight: FontWeight.bold),
               textAlign: TextAlign.center,
             ),
-            const Text(
-                "Lorem ipsum dolor sit amet consectetur. A gravida eros aliquam sed condimentum. Etiam vitae sit scelerisque a eget integer. Quis in dolor amet velit pretium sed vestibulum. Urna ullamcorper eu egestas egestas amet id.",
-                textAlign: TextAlign.center),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Your one-stop shop for everything Apoorv!\n"
+                  "",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  "-  Earn points by playing games",
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                Text(
+                  "-  Exchange points with friends and climb up the\n "
+                  "   leaderboard",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                  textAlign: TextAlign.start,
+                ),
+                Text(
+                  "-  Explore the campus with an interactive virtual\n"
+                  "   Map",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                  textAlign: TextAlign.start,
+                ),
+                Text(
+                  "-  Stay updated with the latest Apoorv related\n"
+                  "   news and event schedules",
+                 style: TextStyle(
+                    fontSize: 16,
+                  ),
+                  textAlign: TextAlign.start,
+                ),
+                Text(
+                  "\n"
+                  "Made with ❤ by team apoorv"
+                  "\n",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
             FilledButton(
               onPressed: () {
                 Navigator.popUntil(context, ModalRoute.withName(WelcomeScreen.routeName));
@@ -43,14 +94,10 @@ class LetsGoPage extends StatelessWidget {
               child: Container(
                 height: 48,
                 alignment: Alignment.center,
-                child: Container(
-                  height: 48,
-                  alignment: Alignment.center,
-                  child: const Text(
-                    'Continue',
-                    style: TextStyle(fontSize: 20),
-                    textAlign: TextAlign.center,
-                  ),
+                child: const Text(
+                  'Continue',
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
                 ),
               ),
             ),
